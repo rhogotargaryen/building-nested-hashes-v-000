b4c77281ba4082778baba1d74e9e023d9a1a34eb
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 07:23:58 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord montague",
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
