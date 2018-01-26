2f11457df30a0b140fcedda39568a2d29db05856
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 07:29:20 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,22 +5,6 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [ {
-        name: "Steven",
-        age: "30",
-        attitude: "confused"
-      },
-      {
-        name: "Nurse",
-        age: "44",
-        attitude: "worried"
-      }
-    ]
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [ {
         name: "Benvolio",
         age: "17",
         attitude: "worried"
@@ -31,9 +15,37 @@ def fourth_challenge
         attitude: "hot-headed"
       }
     ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [ {
+        name: "Steven",
+        age: "30",
+        attitude: "confused"
+      },
+      {
+        name: "Nurse",
+        age: "44",
+        attitude: "worried"
+      }
+      ]
    }
 }
 
 
 
 end
+
+[ {
+  name: "Steven",
+  age: "30",
+  attitude: "confused"
+},
+{
+  name: "Nurse",
+  age: "44",
+  attitude: "worried"
+}
+]
